685fe30602d34dccecdfb722a572a605a0b75abb
Brian Kim <briaansjkim@gmail.com>
2019-12-06 11:04:52 -0500

    implemented romanToInt solution

---
+++ leetcode/RomanToInt.js
@@ -0,0 +1,38 @@
+// given a roman numeral, convert it to an integer
+// input is guaranteed to be within the range from 1 to 3999
+
+var romanToInt = (string) => {
+  var romans = {
+    'I': 1,
+    'IV': 4,
+    'V': 5,
+    'IX': 9,
+    'X': 10,
+    'XL': 40,
+    'L': 50,
+    'XC': 90,
+    'C': 100,
+    'CD': 400,
+    'D': 500,
+    'CM': 900,
+    'M': 1000
+  }
+
+  var romanNum = 0;
+
+  for (var i = 0; i < string.length; i++) {
+    if (romans[string[i]] < romans[string[i + 1]]) {
+      romanNum -= romans[string[i]];
+    } else {
+      romanNum += romans[string[i]];
+    }
+  }
+  return romanNum;
+}
+
+// console.log(romanToInt('I')); // 1
+// console.log(romanToInt('III')); // 2
+// console.log(romanToInt('IV')); // 4
+// console.log(romanToInt('IX')); // 9
+// console.log(romanToInt('LVIII')); // 58
+// console.log(romanToInt('MCMXCIV')); // 1994
