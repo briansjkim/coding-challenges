bbd4d100cad1b1bbee071cff6ab7e00d1dc32ff8
Brian Kim <briaansjkim@gmail.com>
2019-12-12 19:24:50 -0500

    implemented mergeTwoSortedLists

---
+++ leetcode/mergeTwoSortedLists.js
@@ -0,0 +1,28 @@
+// merge two sorted lists and return it as a new list
+
+var mergeTwoSortedLists = (arr1, arr2) => {
+  var result = [];
+  var i = 0;
+  var j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+
+console.log(mergeTwoSortedLists([1,2,4], [1, 3, 4]));
