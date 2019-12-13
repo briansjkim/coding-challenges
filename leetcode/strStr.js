3c66ab84f7e9a27cb57919c5d0402aab1453677b
Brian Kim <briaansjkim@gmail.com>
2019-12-13 11:43:53 -0500

    implemented cheat method for strStr

--- leetcode/strStr.js
+++ leetcode/strStr.js
@@ -1,3 +1,10 @@
 // return the index of the first occurence of needle in haystack, or -1 if needle is not part of haystack;
 
 // edge case: what if needle is an empty string?
+
+// cheating
+var strStr = (haystack, needle) => {
+  return haystack.indexOf(needle);
+}
+
+// console.log(strStr('hello', 'll')); // 2
