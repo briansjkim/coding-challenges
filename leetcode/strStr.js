80494ae824edec1ad261c480546fec5f37e8edb9
Brian Kim <briaansjkim@gmail.com>
2019-12-13 11:52:36 -0500

    implemented strStr

--- leetcode/strStr.js
+++ leetcode/strStr.js
@@ -3,9 +3,9 @@
 // edge case: what if needle is an empty string?
 
 // cheating
-// var strStr = (haystack, needle) => {
-//   return haystack.indexOf(needle);
-// }
+var strStr = (haystack, needle) => {
+  return haystack.indexOf(needle);
+}
 
 // linear time complexity
 var strStr = (haystack, needle) => {
