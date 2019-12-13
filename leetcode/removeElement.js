597164b742e59812cfe4b483ad0560a387265632
Brian Kim <briaansjkim@gmail.com>
2019-12-13 10:45:57 -0500

    init setup for removeElement

---
+++ leetcode/removeElement.js
@@ -0,0 +1,3 @@
+// given an array nums and a value, remove all instances of that value in-place and return the new length
+// do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory
+// order of elements can be changed. it doesn't matter what you leave beyond the new length
