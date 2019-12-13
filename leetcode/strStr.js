d38d7e361ce856b63f42b8d8c82faff26d4312a8
Brian Kim <briaansjkim@gmail.com>
2019-12-13 11:52:02 -0500

    implemeted strStr with linear time complexity

--- leetcode/strStr.js
+++ leetcode/strStr.js
@@ -3,8 +3,24 @@
 // edge case: what if needle is an empty string?
 
 // cheating
+// var strStr = (haystack, needle) => {
+//   return haystack.indexOf(needle);
+// }
+
+// linear time complexity
 var strStr = (haystack, needle) => {
-  return haystack.indexOf(needle);
+  if (needle.length === 0|| needle.length > haystack.length) {
+    return 0;
+  }
+
+  for (var i = 0; i < haystack.length; i++) {
+    let temp = haystack.substring(i, i + needle.length);
+    if (needle === temp) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 // console.log(strStr('hello', 'll')); // 2
+// console.log(strStr('aaaaaa', 'bba')); // -1
