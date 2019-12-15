2cf7ea1f3a4ff09bbe0eb91782cb7ea68d617037
Brian Kim <briaansjkim@gmail.com>
2019-12-15 16:47:16 -0500

    solved lengthOfLongestSubstring in quadratic time

---
+++ leetcode/lengthOfLongestSubstring.js
@@ -0,0 +1,51 @@
+// given a string, find the length of the longest substring without repeating characters
+
+// i: string
+// o: number
+// c: n/a
+// e: empty string, 0
+    // if there are no repeats
+      // add the temp var to longest
+
+
+
+var lengthOfLongestSubstring = (s) => {
+  if (s.length <= 1) {
+    return s.length;
+  }
+
+  var longest = '';
+  var temp = '';
+  var i = 0;
+  var j = 0;
+
+
+  while (i < s.length && j < s.length) {
+    if (temp.indexOf(s[j]) < 0) {
+      temp += s[j];
+      if (j === s.length - 1) {
+        if (longest.length < temp.length) {
+          longest = temp;
+          break;
+        }
+      }
+      j++;
+    } else {
+      if (longest.length < temp.length) {
+        longest = temp;
+      }
+      temp = '';
+      i++;
+      j = i;
+    }
+  }
+  return longest.length;
+}
+
+// console.log(lengthOfLongestSubstring('abcabcbb')); // 3
+// console.log(lengthOfLongestSubstring('bbbbb')); // 1
+// console.log(lengthOfLongestSubstring('pwwkew')); // 3
+// console.log(lengthOfLongestSubstring(' ')); // 1
+// console.log(lengthOfLongestSubstring('')); // 0
+// console.log(lengthOfLongestSubstring('au')); // 2
+// console.log(lengthOfLongestSubstring('dvdf')); // 3
