6e6870a67886c50f7531b23d491ef1a1a21e5de7
Brian Kim <briaansjkim@gmail.com>
2019-12-16 10:07:39 -0500

    init setup for longestPalindrome

---
+++ leetcode/longestPalindrome.js
@@ -0,0 +1,8 @@
+// given a string s
+// find the longest palindromic substring in s
+// you may assume that the max length of s is 1000
+
+// i: string
+// o: string (longest palindromic substring in s)
+// c: n/a
+// e: if the length is 0, return an empty string
