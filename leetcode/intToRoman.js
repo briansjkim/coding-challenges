bc88dbc8b9178fdac7ed41172bab09508bb12a3b
Brian Kim <briaansjkim@gmail.com>
2019-12-16 14:47:08 -0500

    init set up for intToRoman

---
+++ leetcode/intToRoman.js
@@ -0,0 +1,8 @@
+// given an integer, convert it to a roman numeral
+// input is guaranteed to be within the range from 1 to 3999
+
+// i: number
+// o: string (roman)
+// c: n/a
+// e: 4, 9, 40, 90, 400, 900 are written differently
+// 4: IV, 9: IX, XL: 40, XC: 90, CD: 400, CM: 900
