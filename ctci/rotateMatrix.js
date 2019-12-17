54b72e8b8c307ab67dcad8741e2fe5eddaa54f1a
Brian Kim <briaansjkim@gmail.com>
2019-12-17 11:29:50 -0500

    init set up for rotateMatrix

---
+++ ctci/rotateMatrix.js
@@ -0,0 +1,9 @@
+// given an image represented by an NxN matrix
+// where each pixel in the image is presented by an integer
+// write a method to rotate the image by 90 degrees
+// can you do this in place?
+
+//i: NxN matrix
+//o: NxN matrix (rotated 90 degrees)
+//c: rotate matrix in place
+//e: empty matrix
