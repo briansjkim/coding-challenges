3905028c15c520efcd24fd975349e48f1ce80627
Brian Kim <briaansjkim@gmail.com>
2019-12-17 09:40:25 -0500

    init set up for stringCompression

---
+++ ctci/stringCompression.js
@@ -0,0 +1,5 @@
+// implement a method to perform basic string compression using the counts of repeated characters
+// Example:
+  // 'aabcccccaa' would become 'a2b1c5a3'
+// if the "compressed" string would not become smaller than the original string, your method should return the original string
+// you can assume the string only uppercase and lowercase letters
