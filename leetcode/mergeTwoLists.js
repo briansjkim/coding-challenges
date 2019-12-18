e9048d4b0457c4d02700644063fb0c9aac3f6a79
Brian Kim <briaansjkim@gmail.com>
2019-12-18 09:52:27 -0500

    init setup for mergeTwoLists

---
+++ leetcode/mergeTwoLists.js
@@ -0,0 +1,11 @@
+// merge two sorted linked lists and return it as a new list
+// the new list should be made by splicing together the nodes of the first two lists
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var mergeTwoLists = (l1, l2) => {
+
+}
