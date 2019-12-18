6906ced2c78cb5c81d82284c7b27d710f70422a1
Brian Kim <briaansjkim@gmail.com>
2019-12-18 13:18:49 -0500

    implemented countAndSay

--- leetcode/countAndSay.js
+++ leetcode/countAndSay.js
@@ -18,9 +18,39 @@
 //e: n will always be between 1 and 30
 
 // approach:
+  // create a function called countAndSay to return the sequence of integers from the provided integer (n)
+  // create a string variable and assign it to '1' because that's the starting point (result)
+  // iterate from 2 to n ( start at 2 because the result variable already has '1')
+    // create a temporary string variable to get each sequence per iteration (tempStr)
+    // create a counter to state the number of occurrences for each integer (count)
+    // iterate through the result string (iterate through the result string because that will constantly be updated after each iteration through it to equal the number of occurrences for each integer)
+      // increment the counter so that each character has at least one occurrence regardless of whether or not the next char matches the current char
+      // reassign count to be 0
+      // if the next character doesn't match the current character
+        // assign tempStr to equal counter + the current character
+    // assign tempStr to result at the end of every inner iteration
+  // return result
 
 var countAndSay = (n) => {
   var result = '1';
-};
+  if ( (n < 1) || (n > 30)) {
+    return null;
+  }
+  if (n < 2) {
+    return result;
+  }
 
-// console.log(countAndSay(1)); // '1'
+  for (var i = 2; i <= n; i++) {
+    var tempStr = '';
+    var count = 0;
+    for (var j = 0; j < result.length; j++) {
+      count++;
+      if (result[j] !== result[j + 1]) {
+        tempStr += count + result[j];
+        count = 0;
+      }
+    }
+    result = tempStr;
+  }
+  return result;
+};
