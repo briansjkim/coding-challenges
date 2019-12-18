844e8ee6cc70e0f910b3854cb09d59e7b86bcff7
Brian Kim <briaansjkim@gmail.com>
2019-12-18 13:35:00 -0500

    init set up for addTwoNumbers

---
+++ leetcode/addTwoNumbers.js
@@ -0,0 +1,15 @@
+// you are given two non-empty linked lists representing two non-negative integers
+// the digits are stored in REVERSE order and each of their nodes contain a single digit
+// add the two numbers and return it as a linked list
+// you may assume the two numbers don't contain any leading zero, except the number 0 itself
+
+/**
+ * Example:
+ * Input : (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 645 = 807
+ */
+
+var addTwoNumbers = (l1, l2) => {
+
+}
