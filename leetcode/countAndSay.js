ecbb7a49b7c4d26acab73b2a69c521d72e54d4b7
Brian Kim <briaansjkim@gmail.com>
2019-12-18 12:04:14 -0500

    init set up for countAndSay

---
+++ leetcode/countAndSay.js
@@ -0,0 +1,13 @@
+/**
+ * the count-and-say sequence is the sequence of integers with the first five terms as following:
+ * 1. 1
+ * 2. 11
+ * 3. 21
+ * 4. 1211
+ * 5. 111221
+ *
+ * given an integer n where 1 <= n <= 30, generate the nth term of the count-and-say sequence.
+ * you can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit
+ *
+ * note: each term of the sequence of integers will be represented as a string
+ */
