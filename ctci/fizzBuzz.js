f271b2a79d4fbd055cd577c18f6a94564d86ec64
Brian Kim <briaansjkim@gmail.com>
2019-12-17 19:58:37 -0500

    implemented fizzBuzz

--- ctci/fizzBuzz.js
+++ ctci/fizzBuzz.js
@@ -5,5 +5,17 @@
 // for numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number
 
 var FizzBuzz = () => {
-
+  for (var i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+      console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
 }
+
+FizzBuzz();
