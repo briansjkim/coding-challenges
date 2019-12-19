99f2a92a0d38454bfd846154f05825aa4767c918
Brian Kim <briaansjkim@gmail.com>
2019-12-19 10:31:56 -0500

    init set up for lengthOfLastWord

---
+++ leetcode/lengthOfLastWord.js
@@ -0,0 +1,11 @@
+// given a string s consists of upper/lower-case alphabets and empty space characters ' ',
+// return the length of last word in the string
+// if the last word doesn't exist, return 0
+// a word is defined as a character sequence consisting of non-space characters only
+
+var lengthOfLastWord = function(s) {
+}
+
+console.log(lengthOfLastWord('a ')); // 1
+console.log(lengthOfLastWord('     Hello      World    ')); // 5
+console.log(lengthOfLastWord('Hello World')); // 5
