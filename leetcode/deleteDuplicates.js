54d913ccde51d0f20f4e1f5147edc2f87dff4a8a
Brian Kim <briaansjkim@gmail.com>
2019-12-26 12:33:03 -0500

    implemented deleteDuplicates

--- leetcode/deleteDuplicates.js
+++ leetcode/deleteDuplicates.js
@@ -1,4 +1,14 @@
 // given a sorted linked list, delete all duplicates such that each element appear only once
 
 var deleteDuplicates = (head) => {
+  var list = head;
+
+  while (list) {
+    if (list.next !== null && list.val === list.next.val) {
+      list.next = list.next.next;
+    } else {
+      list = list.next;
+    }
+  }
+  return head;
 }
