779626bf0a10ab07f0753e7ce23e78f30cfbccd0
Brian Kim <briaansjkim@gmail.com>
2019-12-27 10:10:09 -0500

    implemented pathSum

--- leetcode/pathSum.js
+++ leetcode/pathSum.js
@@ -1,2 +1,22 @@
 var hasPathSum = (root, sum) => {
+  if (!root) {
+    return false;
+  }
+
+  // if we reach a leaf
+  if (!root.left && !root.right) {
+    // return whether or not the value of the leaf is equal to what's left of the sum
+    return sum === root.val;
+  }
+
+  // as we traverse the left,
+  // subtract root.val from the sum
+  var traverseLeft = hasPathSum(root.left, sum - root.val);
+  // same with traversing the right
+  var traverseRight = hasPathSum(root.right, sum - root.val);
+  // if the sum reached 0 in either left or right
+    // true will be returned
+  // if the sum didn't reach 0 in either left or right
+    // false will be returned
+  return traverseLeft || traverseRight;
 }
