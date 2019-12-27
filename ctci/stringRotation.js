12d9dcad726db1c5344bda4ce4d95a003ff1723f
Brian Kim <briaansjkim@gmail.com>
2019-12-27 11:03:38 -0500

    init set up for stringRotation

---
+++ ctci/stringRotation.js
@@ -0,0 +1,8 @@
+// assume you have a method 'isSubstring' which checks if one word is a substring of another
+// given 2 strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring
+// Example
+  // waterbottle is a rotation of erbottlewat
+
+var stringRotation = (s1, s2) => {
+
+}
