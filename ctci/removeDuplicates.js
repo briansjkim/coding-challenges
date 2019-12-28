679e1849fbaa32d01f1910ae7eb7da292227aa0f
Brian Kim <briaansjkim@gmail.com>
2019-12-28 10:59:53 -0500

    init set up for removeDuplicates

---
+++ ctci/removeDuplicates.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    return this;
+  }
+
+  removeDuplicates() {
+
+  }
+}
