4d62d951c3080ee8dfd32fa55709634d0464d4ba
Brian Kim <briaansjkim@gmail.com>
2019-12-30 11:37:48 -0500

    init set up for kthToLast

---
+++ ctci/kthToLast.js
@@ -0,0 +1,31 @@
+// implement an algorithm to find the kth to last element of a singly linked list
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    return this;
+  }
+}
+
+var kthToLast = (list, n) => {
+
+}
