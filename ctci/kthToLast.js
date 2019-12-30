85b36a1342b56656984acc6e3a3584e868791e76
Brian Kim <briaansjkim@gmail.com>
2019-12-30 11:54:58 -0500

    implemented kthToLast

--- ctci/kthToLast.js
+++ ctci/kthToLast.js
@@ -26,6 +26,39 @@ class LinkedList {
   }
 }
 
-var kthToLast = (list, n) => {
+var kthToLast = (list, k) => {
+  if (!list.head) {
+    return null;
+  }
+  var p1 = list.head;
+  var p2 = list.head;
 
+  for (var i = 0; i < k; i++) {
+    // if k is greater than the length of the list
+      // return null
+    if (!p1) {
+      return null;
+    }
+    p1 = p1.next;
+  }
+
+  while (p1 !== null) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  return p2;
 }
+
+var list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5);
+list.add(6);
+list.add(7);
+
+// console.log(kthToLast(list, 3)); // 5
+// console.log(kthToLast(list, 2)); // 6
+// console.log(kthToLast(list, 4)); // 4
