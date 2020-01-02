56e2fae4320461300a1f3edc3b678ff268a4ca78
Brian Kim <briaansjkim@gmail.com>
2020-01-02 11:07:19 -0500

    added another example

--- leetcode/sameTree.js
+++ leetcode/sameTree.js
@@ -1,10 +1,12 @@
 // given two binary trees, write a function to check if they are the same or not
 // two binary trees are considered the same if they are structurally identical and the nodes have the same value
 
-// Example:
+// Examples:
   // Input: [1, 2, 3], [1, 2, 3]
     // Output: True
   // Input: [1, 2], [1, null, 2]
+    // Output: False
+  // Input: [1, 2, 1], [1, 1, 2]
     // Output: False
 
 var isSameTree = function(p, q) {
