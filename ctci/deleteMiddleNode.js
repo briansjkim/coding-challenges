30c0a2980aeead1dfba4e9eb0b4cb8c2b1b93532
Brian Kim <briaansjkim@gmail.com>
2020-01-02 20:04:13 -0500

    implemented deleteMiddleNode

--- ctci/deleteMiddleNode.js
+++ ctci/deleteMiddleNode.js
@@ -2,3 +2,43 @@
 
 // Input: the node c from the linked list a -> b -> c -> d -> e -> f
 // Output: nothing is returned, but the new linked list looks like a -> b -> d -> e -> f
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    return this;
+  }
+}
+
+var removeMidNode = (node, list) => {
+  if (!list.head) {
+    return null;
+  }
+  var pointer = list.head;
+  while (pointer) {
+    if (pointer.next.val === node.val) {
+      pointer.next = pointer.next.next;
+      break;
+    }
+    pointer = pointer.next;
+  }
+};
