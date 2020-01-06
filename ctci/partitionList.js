3fe8b29a808d59e675497ad2ea091caf9193891f
Brian Kim <briaansjkim@gmail.com>
2020-01-06 15:48:19 -0500

    init set up for partitionList

---
+++ ctci/partitionList.js
@@ -0,0 +1,7 @@
+// write code to partition a linked list around a value x,
+// such that all nodes less than x come before all nodes greater than or equal to x
+// Note: the partition element x can appear anywhere in the 'right partition'; it doesn't need to appear between the left and right partitions
+
+// Example:
+// Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1
+// Output: 3 -> 1 -> 2    10 -> 5 -> 5 -> 8
