710c148f12d0e84041e1787c05b288c2e9f88d48
Brian Kim <briaansjkim@gmail.com>
2020-01-06 11:14:38 -0500

    added examples

--- leetcode/symmetricTree.js
+++ leetcode/symmetricTree.js
@@ -1,3 +1,20 @@
 // given a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center)
 
+// Example:
+// Input: [1, 2, 2, 3, 4, 4, 3];
+// Root: [1]
+// Left: [2, 3, 4]
+// Right: [2, 4, 3]
+// Output: True
+// Input: [1, 2, 2, null, 3, null, 3]
+// Root: [1]
+// Left: [2, null, 3]
+// Right: [2, null, 3]
+// Output: False
+
+// i: binary tree
+// o: boolean (whether the tree is a mirror of itself or not)
+// c:
+// e:
+
 var isSymmetric = root => {};
