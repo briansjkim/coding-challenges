179f0a722ae33b2f9a6a92ac9497fac05b8b8841
Brian Kim <briaansjkim@gmail.com>
2020-01-06 16:57:46 -0500

    init set up for partitionList

---
+++ leetcode/partitionList.js
@@ -0,0 +1,9 @@
+// given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x
+// you should preserve the original relative order of the nodes in each of the two partitions
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+var partition = function(head, x) {};
