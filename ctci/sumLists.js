8c4233c55886584f8353b966ae9aa8e76bc4b734
Brian Kim <briaansjkim@gmail.com>
2020-01-07 15:58:11 -0500

    init set up for sumLists w/ example

---
+++ ctci/sumLists.js
@@ -0,0 +1,8 @@
+// you have two numbers represented by a linked list, where each node contains a single digit
+// the digits are stored in REVERSE order, such that the 1's digit is at the head of the list.
+// write a function that adds the two numbers and returns the sum as a linked list
+// NOTE: Not allowed to 'cheat' and just convert the linked list to an integer
+
+// Examples:
+// Input: (7 -> 1 -> 6) + (5 -> 9 -> 2) => 617 + 295
+// Output: (2 -> 1 -> 9) => 912
