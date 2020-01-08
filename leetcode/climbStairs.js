eed3e07fc0240ba2a840186a6713b0b2b0bb4a16
Brian Kim <briaansjkim@gmail.com>
2020-01-08 11:44:45 -0500

    implemented climbStairs

--- leetcode/climbStairs.js
+++ leetcode/climbStairs.js
@@ -17,4 +17,22 @@
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
 
-var climbStairs = n => {};
+// basically, it's fibonacci
+var climbStairs = n => {
+  // create an array to store the number of steps per n
+  var steps = [];
+  // solves for 0
+  steps[0] = 1;
+  // solves for 1
+  steps[1] = 1;
+
+  // start at 2 because we already solved 0 and 1
+  for (var i = 2; i <= n; i++) {
+    // the number of steps for n will be based on the sum of the number steps from i - 1 and i - 2
+    // n = 2 => steps[1] + steps[0] = steps[2] = 2
+    // n = 3 => steps[2] + steps[1] = steps[3] = 3
+    // n = 4 => steps[3] + steps[2] = steps[4] = 5
+    steps[i] = steps[i - 1] + steps[i - 2];
+  }
+  return steps[n];
+};
