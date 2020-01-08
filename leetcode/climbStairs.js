e827c01632df06092f32ed58b6eb7575a6048f2b
Brian Kim <briaansjkim@gmail.com>
2020-01-08 11:15:41 -0500

    init set up for climbStairs with an example

---
+++ leetcode/climbStairs.js
@@ -0,0 +1,10 @@
+// you are climbing a stair case. it takes n steps to reach to the top
+// each time you can either clime 1 or 2 steps. in how many distinct ways can you climb to the top?
+// Note: Given n will be a positive integer
+
+// Example 1:
+// Input: 2
+// Output: 2
+// Explanation: There are two ways to climb to the top
+// 1. 1 step + 1 step
+// 2. 2 steps
