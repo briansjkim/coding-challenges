3a6e0e6686b1f2cebb03ea69a5e1819dd7419b7b
Brian Kim <briaansjkim@gmail.com>
2020-01-09 12:07:47 -0500

    init set up for minDepth

---
+++ leetcode/minDepth.js
@@ -0,0 +1,7 @@
+// given a binary tree, find its minimum depth
+// the minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node
+// Note: a leaf is a node with no children
+
+// Example:
+// given a binary tree [3, 9, 20, null, null, 15, 7]
+// its minimum depth = 2
