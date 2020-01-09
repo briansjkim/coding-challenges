dac93fbe94a219503f2aa7b240aca5e0fd6c2806
Brian Kim <briaansjkim@gmail.com>
2020-01-09 12:10:12 -0500

    implemented minDepth

--- leetcode/minDepth.js
+++ leetcode/minDepth.js
@@ -5,3 +5,22 @@
 // Example:
 // given a binary tree [3, 9, 20, null, null, 15, 7]
 // its minimum depth = 2
+
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+var minDepth = root => {
+  if (!root) {
+    return 0;
+  }
+  if (!root.left) {
+    return minDepth(root.right) + 1;
+  }
+  if (!root.right) {
+    return minDepth(root.left) + 1;
+  }
+
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+};
