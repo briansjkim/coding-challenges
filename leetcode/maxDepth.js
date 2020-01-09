04978b8f7d4701e7555390ae929dd7161a7b9160
Brian Kim <briaansjkim@gmail.com>
2020-01-09 11:56:09 -0500

    added test case

--- leetcode/maxDepth.js
+++ leetcode/maxDepth.js
@@ -28,3 +28,13 @@ var maxDepth = function(root) {
   // could also just do
   // return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
+
+var tree = new TreeNode(3);
+tree.left = new TreeNode(9);
+tree.right = new TreeNode(20);
+tree.left.left = null;
+tree.left.right = null;
+tree.right.left = new TreeNode(15);
+tree.right.right = new TreeNode(7);
+
+// console.log(maxDepth(tree)); // 3
