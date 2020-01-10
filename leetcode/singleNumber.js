424a6b2281675844625eeea1c920a20bddedfb89
Brian Kim <briaansjkim@gmail.com>
2020-01-10 13:14:43 -0500

    init setup for singleNumber

---
+++ leetcode/singleNumber.js
@@ -0,0 +1,7 @@
+// given a non-empty array of integers, every element appears twice except for one. Find that single one
+// Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+// I: non-empty array of integers
+// O: Number that isn't repeating
+// C: Linear time complexity and No extra memory
+// E: [2, 2, 1] => 1; [4, 1, 2, 1, 2] => 4
