eb8b8d360836365f3ac0aaeab7b38d97b7b164e3
Brian Kim <briaansjkim@gmail.com>
2020-01-10 13:17:14 -0500

    implemented initial solution

--- leetcode/singleNumber.js
+++ leetcode/singleNumber.js
@@ -5,3 +5,24 @@
 // O: Number that isn't repeating
 // C: Linear time complexity and No extra memory
 // E: [2, 2, 1] => 1; [4, 1, 2, 1, 2] => 4
+
+// implemented using an object
+var singleNumber = nums => {
+  var storage = {};
+  for (var i = 0; i < nums.length; i++) {
+    if (storage[nums[i]]) {
+      storage[nums[i]]++;
+    } else {
+      storage[nums[i]] = 1;
+    }
+  }
+
+  for (var key in storage) {
+    if (storage[key] === 1) {
+      return key;
+    }
+  }
+};
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
