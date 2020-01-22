86ac75f207ede0d6c34d3cc2f3279464fd060fa0
Brian Kim <briaansjkim@gmail.com>
2020-01-22 17:46:34 -0500

    added listNode function and LinkedList class

--- ctci/palindromeList.js
+++ ctci/palindromeList.js
@@ -7,6 +7,29 @@
 // Input: 1 -> 2 -> 3 -> 1 -> 2
 // Output: false
 
-var check = function (list) {
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(val) {
+    var newNode = new ListNode(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+}
+
+var palindromeList = function (list) {
 
 }
