1c30853bb67095489186e90078bc7eafd85703f1
Brian Kim <briaansjkim@gmail.com>
2020-01-22 17:44:58 -0500

    init function setup

--- ctci/palindromeList.js
+++ ctci/palindromeList.js
@@ -1,8 +1,12 @@
 // Implement a function to check if a linked list is a palindrome
 
 // Example:
-  // Input: 1 -> 2 -> 3 -> 2 -> 1
-  // Output: true
+// Input: 1 -> 2 -> 3 -> 2 -> 1
+// Output: true
 // Example 2:
-  // Input: 1 -> 2 -> 3 -> 1 -> 2
-  // Output: false
+// Input: 1 -> 2 -> 3 -> 1 -> 2
+// Output: false
+
+var check = function (list) {
+
+}
