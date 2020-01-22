bdb8ec8ef637b37c5a26312a398d6b289cdf6062
Brian Kim <briaansjkim@gmail.com>
2020-01-22 16:23:07 -0500

    init function setup

--- leetcode/pascalTriangle.js
+++ leetcode/pascalTriangle.js
@@ -1,8 +1,13 @@
 // given a non-negative integer numRows, generate the first numRows of Pascal's triangle
 
 // Example:
-  // Input: 5
-  // Output: [ [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1] ]
+// Input: 5
+// Output: [ [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1] ]
 // Example 2:
-  // Input: 7
-  // Output: [ [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1], [1, 6, 15, 20, 15, 6, 1] ]
+// Input: 7
+// Output: [ [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1], [1, 6, 15, 20, 15, 6, 1] ]
+
+
+var generate = function (numRows) {
+
+}
