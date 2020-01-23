79f6a6adda4bee30c269743606e4098e31b819f2
Brian Kim <briaansjkim@gmail.com>
2020-01-23 11:29:25 -0500

    init set up for intersection and an example

---
+++ ctci/intersection.js
@@ -0,0 +1,8 @@
+// given two (singly) linked lists, determine if the two lists intersect. Return the intersecting node
+// Note that the intersection is defined based on reference, not value.
+// That is, if the kth node of the first linked list is the exact same node (by reference) as the jth node of the second linked list, then they are intersecting
+
+// Example
+  // Input: L1: 1 -> 2 -> 3 -> 4 -> 5 & L2: 5 -> 4 -> 3 -> 2 -> 1
+  // Output: 3
+
