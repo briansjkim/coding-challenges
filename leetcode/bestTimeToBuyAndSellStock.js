dbce18482dd0f8fab27d4f7824d9a4bd1871a1b5
Brian Kim <briaansjkim@gmail.com>
2020-01-23 10:04:11 -0500

    added example

--- leetcode/bestTimeToBuyAndSellStock.js
+++ leetcode/bestTimeToBuyAndSellStock.js
@@ -2,3 +2,9 @@
 // if you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
 // Note: You cannot sell a stock before you buy one.
+
+// Example:
+  // Input: [7, 1, 5, 3, 6, 4]
+  // Output: 5
+  // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5
+    // Note: 7 - 1 = 6, as selling price needs to be larger than buying price
