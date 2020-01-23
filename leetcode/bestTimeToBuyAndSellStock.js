b063723d65b2e12289f9e86f8d0a5d1440d2a205
Brian Kim <briaansjkim@gmail.com>
2020-01-23 10:29:07 -0500

    added 3rd implementation

--- leetcode/bestTimeToBuyAndSellStock.js
+++ leetcode/bestTimeToBuyAndSellStock.js
@@ -43,3 +43,19 @@ var maxProfit = function (prices) {
   }
   return max;
 }
+
+// Linear
+var maxProfit = function (prices) {
+  var min = Number.MAX_SAFE_INTEGER;
+  var max = 0;
+
+  for (var i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    } else if (prices[i] - min > max) {
+      max = prices[i] - min;
+    }
+  }
+
+  return max;
+}
