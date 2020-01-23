794869a29aba1caf73d2f2ee1f8068443b9cfe3b
Brian Kim <briaansjkim@gmail.com>
2020-01-23 10:04:50 -0500

    added second example

--- leetcode/bestTimeToBuyAndSellStock.js
+++ leetcode/bestTimeToBuyAndSellStock.js
@@ -8,3 +8,7 @@
   // Output: 5
   // Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5
     // Note: 7 - 1 = 6, as selling price needs to be larger than buying price
+// Example 2:
+  // Input: [7, 6, 4, 3, 1]
+  // Output: 0
+  // Explanation: In this case, no transaction is done because the max profit is 0
