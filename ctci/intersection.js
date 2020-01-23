cc80b258c2a8d2a6cc98d23c39aa6948596831a4
Brian Kim <briaansjkim@gmail.com>
2020-01-23 11:30:50 -0500

    init function setup

--- ctci/intersection.js
+++ ctci/intersection.js
@@ -3,8 +3,12 @@
 // That is, if the kth node of the first linked list is the exact same node (by reference) as the jth node of the second linked list, then they are intersecting
 
 // Example
-  // Input: L1: 1 -> 2 -> 3 -> 4 -> 5 & L2: 5 -> 4 -> 3 -> 2 -> 1
-  // Output: 3
+// Input: L1: 1 -> 2 -> 3 -> 4 -> 5 & L2: 5 -> 4 -> 3 -> 2 -> 1
+// Output: 3
 // Example 2:
-  // Input: L1: 1 -> 2 -> 3 -> 4 -> 5 & L2: 5 -> 4 -> 1 -> 3 -> 2
-  // Output: Null
+// Input: L1: 1 -> 2 -> 3 -> 4 -> 5 & L2: 5 -> 4 -> 1 -> 3 -> 2
+// Output: Null
+
+var intersection = function (l1, l2) {
+
+}
