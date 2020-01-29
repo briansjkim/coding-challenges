b74f1671c922e34e4cb70ca62bc07c604b44162f
Brian Kim <briaansjkim@gmail.com>
2020-01-29 10:19:25 -0500

    added example

--- leetcode/bestTimeToBuyAndSellStock2.js
+++ leetcode/bestTimeToBuyAndSellStock2.js
@@ -3,3 +3,11 @@
 // Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
 
 // Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+// Example 1:
+  // I: [7, 1, 5, 3, 6, 4]
+  // O: 7
+  // Explanation: Buy on day 2 (price = 1) and sell on day 3 (prices = 5), profit = 5 - 1 = 4
+    // Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6 -3 = 3
+    // Then 4 + 3 = 7
+
