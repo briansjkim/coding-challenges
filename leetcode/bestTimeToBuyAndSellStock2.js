96410b3671ccfea36c22b4c40793255de1bb35ec
Brian Kim <briaansjkim@gmail.com>
2020-01-29 10:20:52 -0500

    added another example

--- leetcode/bestTimeToBuyAndSellStock2.js
+++ leetcode/bestTimeToBuyAndSellStock2.js
@@ -11,3 +11,8 @@
     // Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6 -3 = 3
     // Then 4 + 3 = 7
 
+// Example 2:
+  // I: [1, 2, 3, 4, 5]
+  // O: 4
+  // Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5 - 1 = 4
+    // Can't buy on day 1, buy on day 2, and sell them later since that's engaging in multiple transactions at the same time. Must sell stock before buying again
