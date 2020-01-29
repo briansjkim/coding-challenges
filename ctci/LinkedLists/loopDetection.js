696ff0248a359de136629f23a51085bac302d6ac
Brian Kim <briaansjkim@gmail.com>
2020-01-29 11:00:56 -0500

    added example

--- ctci/LinkedLists/loopDetection.js
+++ ctci/LinkedLists/loopDetection.js
@@ -1,1 +1,6 @@
 // given a linked list which might contain a loop, implement an algorithm that returns the node at the beginning of the loop (if one exists)
+
+// Example:
+  // I: A -> B -> C -> D -> E -> C (the same C as earlier)
+  // O: C
+
