58336ce32f2128a607d2c4f64a764c4eb1012c55
Brian Kim <briaansjkim@gmail.com>
2020-01-29 11:53:29 -0500

    added my own example

--- leetcode/linkedListCycle.js
+++ leetcode/linkedListCycle.js
@@ -17,3 +17,7 @@
 // O: false
 // Explanation: There isn't a cycle in the linked list
 
+// Example 4:
+// I: head = [1, 3, 6, 2], pos = -1
+// O: false
+// Explanation: There isn't a cycle in the linked list
