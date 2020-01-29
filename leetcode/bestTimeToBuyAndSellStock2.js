7b011513888fea27e80f994f44f59e4b3e0fb0a6
Brian Kim <briaansjkim@gmail.com>
2020-01-29 10:28:16 -0500

    added and passed the test cases

--- leetcode/bestTimeToBuyAndSellStock2.js
+++ leetcode/bestTimeToBuyAndSellStock2.js
@@ -48,3 +48,8 @@ const maxProfit = function (prices) {
   }
   return max;
 }
+
+// console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 7
+// console.log(maxProfit([1, 2, 3, 4, 5])); // 4
+// console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+// console.log(maxProfit([1, 4, 2, 5, 9])); // 10
