ed11306af4459551bdca50daf9391cb8610a2c87
Brian Kim <briaansjkim@gmail.com>
2020-01-29 11:51:40 -0500

    added example

--- leetcode/linkedListCycle.js
+++ leetcode/linkedListCycle.js
@@ -2,3 +2,10 @@
 // to represent a cycle in the given linked list, we use an integer 'pos' which represents the position (0-indexed)
 // in the linked list where tail connects to. If 'pos' is -1, then there is no cycle in the linked list
 
+// Example 1:
+// I: head = [3, 2, 0, -4], pos = 1
+// O: true
+// Explanation: There is a cycle in the linked list, where tail connects to the second node (pos = 1)
+
+
+
