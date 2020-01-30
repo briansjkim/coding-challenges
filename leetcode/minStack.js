73c3d7a1beeacf37f8b4570ce0440c5bf0799013
Brian Kim <briaansjkim@gmail.com>
2020-01-30 12:02:45 -0500

    added test cases

--- leetcode/minStack.js
+++ leetcode/minStack.js
@@ -49,3 +49,11 @@ MinStack.prototype.getMin = function () {
   }
 }
 
+// var minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// console.log(minStack.getMin()); // -3
+// minStack.pop();
+// console.log(minStack.top()); // 0
+// console.log(minStack.getMin()); // -2
