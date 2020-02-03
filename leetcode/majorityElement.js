c586f5a54f79f3428c7d9c6977d2557aabc655db
Brian Kim <briaansjkim@gmail.com>
2020-02-03 08:15:37 -0500

    added an example

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -1,2 +1,8 @@
 // Given an array of size n, find the majority element. The majority element is the element that appears more than n / 2 times
 // You may always assume that the array is non-empty and the majority element always exists in the array
+
+// Example 1:
+// I: [3, 2, 3]
+// O: 3
+// Explanation: n = 3, n / 2 = ~1 and 3 appears more than once in the input array
+
