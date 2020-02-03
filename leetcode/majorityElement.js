39dc2262e39033a30d31f5e1ec6247a097df811f
Brian Kim <briaansjkim@gmail.com>
2020-02-03 08:16:14 -0500

    added 2nd example

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -6,3 +6,9 @@
 // O: 3
 // Explanation: n = 3, n / 2 = ~1 and 3 appears more than once in the input array
 
+// Example 2:
+// I: [2, 2, 1, 1, 1, 2, 2]
+// O: 2
+// Explanation: n = 7, n / 2 = ~3 and 2 appears more than three times in the input array
+
+
