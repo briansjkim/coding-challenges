9d3665cdeb60d95b0ead64c9d037e1c134bf33f8
Brian Kim <briaansjkim@gmail.com>
2020-02-03 08:13:46 -0500

    init setup for majorityElement

---
+++ leetcode/majorityElement.js
@@ -0,0 +1,2 @@
+// Given an array of size n, find the majority element. The majority element is the element that appears more than n / 2 times
+// You may always assume that the array is non-empty and the majority element always exists in the array
