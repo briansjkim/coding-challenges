a08576b288bbd4fd3ceebe8de82c7cfb9f82277b
Brian Kim <briaansjkim@gmail.com>
2020-02-03 08:44:43 -0500

    init class stack

--- ctci/stacksAndQueues/stackMin.js
+++ ctci/stacksAndQueues/stackMin.js
@@ -1,2 +1,8 @@
 // How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element?
 // Push, pop, and min should all operate in O(1) time.
+
+class Stack {
+  constructor() {
+    this.storage = [];
+  }
+}
