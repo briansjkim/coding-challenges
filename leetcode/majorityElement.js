1036ed4aeb86e02c855b021614fccb091a90ae6b
Brian Kim <briaansjkim@gmail.com>
2020-02-03 08:19:23 -0500

    added ioce and my approach

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -11,4 +11,24 @@
 // O: 2
 // Explanation: n = 7, n / 2 = ~3 and 2 appears more than three times in the input array
 
+// I: Array of numbers
+// O: Majority element (num that appears more than n / 2 times)
+// C: N/a
+// E:
+
+// Approach
+  // create a function to find the majority element
+  // calc n / 2 to find how many times each num in input array must occur to be the majority element (majority)
+  // create an object to hold each number (storage)
+  // iterate through the input array
+    // for each number
+      // if the number is in storage
+        // increment its value by 1
+      // else
+        // set its value to 1
+  // iterate through storage
+    // for each key
+      // if its value is greater than majority
+        // return the key
+
 
