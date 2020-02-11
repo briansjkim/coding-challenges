9c8ac4f59c428a21fb8f7a7c65af4e2571cbfe43
Brian Kim <briaansjkim@gmail.com>
2020-02-11 11:32:20 -0500

    added note

--- leetcode/deleteNodeInLinkedList.js
+++ leetcode/deleteNodeInLinkedList.js
@@ -11,3 +11,11 @@ Input: head = [4,5,1,9], node = 1
 Output: [4,5,9]
 Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
 */
+
+/**
+Note:
+The linked list will have at least two elements.
+All of the nodes' values will be unique.
+The given node will not be the tail and it will always be a valid node of the linked list.
+Do not return anything from your function.
+ */
