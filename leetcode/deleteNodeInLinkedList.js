39f04593658275356c9b2d7003c85b83426475c0
Brian Kim <briaansjkim@gmail.com>
2020-02-11 11:35:20 -0500

    implemented deleteNode

--- leetcode/deleteNodeInLinkedList.js
+++ leetcode/deleteNodeInLinkedList.js
@@ -18,8 +18,9 @@ The linked list will have at least two elements.
 All of the nodes' values will be unique.
 The given node will not be the tail and it will always be a valid node of the linked list.
 Do not return anything from your function.
- */
+*/
 
 function deleteNode(node) {
-
+  node.val = node.next.val;
+  node.next = node.next.next;
 }
