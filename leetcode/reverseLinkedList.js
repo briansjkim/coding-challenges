f1824aaa5f1092d3187f6b01f671cde2ef64ce0d
Brian Kim <briaansjkim@gmail.com>
2020-02-13 10:45:21 -0500

    init function setup

--- leetcode/reverseLinkedList.js
+++ leetcode/reverseLinkedList.js
@@ -8,3 +8,7 @@ Output: 5->4->3->2->1->NULL
 
 // Follow up:
 // A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+var reverseList = function (head) {
+
+}
