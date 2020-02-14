664d0bcd670687a6321e4cbaae6dd029b3cbf301
Brian Kim <briaansjkim@gmail.com>
2020-02-14 11:34:51 -0500

    added test case

--- leetcode/reverseLinkedList.js
+++ leetcode/reverseLinkedList.js
@@ -30,3 +30,11 @@ var reverseList = function (head) {
 
   return prev;
 }
+
+var list = new ListNode(1);
+list.next = new ListNode(2);
+list.next.next = new ListNode(3);
+list.next.next.next = new ListNode(4);
+list.next.next.next.next = new ListNode(5);
+// console.log(reverseList(list));
+
