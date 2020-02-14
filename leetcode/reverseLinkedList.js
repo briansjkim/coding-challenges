71af78b5925a8db31f86425a7f15b65748a0ff0c
Brian Kim <briaansjkim@gmail.com>
2020-02-14 11:34:16 -0500

    created listnode function

--- leetcode/reverseLinkedList.js
+++ leetcode/reverseLinkedList.js
@@ -9,6 +9,11 @@ Output: 5->4->3->2->1->NULL
 // Follow up:
 // A linked list can be reversed either iteratively or recursively. Could you implement both?
 
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 // iterative solution
 var reverseList = function (head) {
   if (!head) { return null; }
