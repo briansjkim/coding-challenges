4f8ef78b4c26a9e5e3b4ca166d983b1b62473a0e
Brian Kim <briaansjkim@gmail.com>
2020-03-03 16:52:32 -0500

    init setup

---
+++ leetcode/binaryTreeLevelOrderTraversal2.js
@@ -0,0 +1,10 @@
+// Given a binary tree, return the bottom-up level order traversal of its nodes' values (left to right, level by level, leaf to root)
+
+// Example:
+// Input: Tree: [3, 9, 20, null, null, 15, 7]
+// Output: [ [15, 7], [9, 20], [3] ]
+
+// Exmaple 2:
+// Input: Tree: [3, 9, 20, 7, 13, 15, 7]
+// Output: [ [7, 13], [15, 7], [9, 20], [3] ]
+
