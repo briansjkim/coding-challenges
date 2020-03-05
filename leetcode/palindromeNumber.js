9a5bdc7102ca094116344cba1ba5514001bfafcd
Brian Kim <briaansjkim@gmail.com>
2020-03-04 19:42:43 -0500

    leetcode practice

--- leetcode/palindromeNumber.js
+++ leetcode/palindromeNumber.js
@@ -1,7 +1,7 @@
 // determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward
 
 
-var isPalindrome = function(x) {
+var isPalindrome = function (x) {
   var numString = '' + x;
   var result = '';
   for (var i = numString.length - 1; i >= 0; i--) {
@@ -9,6 +9,22 @@ var isPalindrome = function(x) {
   }
 
   return result === numString;
+}
+
+// slightly faster solution
+var isPalindrome = function (x) {
+  // negative numbers aren't palindromes
+  if (x < 0) { return false; }
+  // if x is a single digit, it is a palindrome
+  if (x < 10) { return true; }
+
+  var numString = String(x);
+  var reverseNum = '';
+
+  for (var i = numString.length - 1; i >= 0; i--) {
+    reverseNum += numString[i];
+  }
+  return reverseNum === numString;
 }
 
 
