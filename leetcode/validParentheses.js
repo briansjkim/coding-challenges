64140e7a75953c01743077a0282f11f740cf5886
Brian Kim <briaansjkim@gmail.com>
2020-03-05 11:47:36 -0500

    leetcode practice

--- leetcode/validParentheses.js
+++ leetcode/validParentheses.js
@@ -1,11 +1,11 @@
 // given a string containing just the characters '(', ')', '{', '}', '[', ']' and determine if the input string is valid
 // an input string is valid if:
-  // open brackets must be closed by the same type of brackets
-  // open brackets must be closed in the correct order
+// open brackets must be closed by the same type of brackets
+// open brackets must be closed in the correct order
 // an empty string is also considered valid
 
 var validParentheses = (string) => {
-  if (string.length === 0)  return true;
+  if (string.length === 0) return true;
 
   // parens will be a stack
   var parens = [];
@@ -27,9 +27,29 @@ var validParentheses = (string) => {
   return false;
 };
 
-console.log(validParentheses('')); // true
-console.log(validParentheses('()')); // true
-console.log(validParentheses('()[]{}')); // true
-console.log(validParentheses('(]')); // false
-console.log(validParentheses('([)]')); // false
-console.log(validParentheses('{[]}')); // true
+var isValid = function (s) {
+  if (s.length < 1) { return true; }
+  var storage = [];
+
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      storage.push(')');
+    } else if (s[i] === '[') {
+      storage.push(']');
+    } else if (s[i] === '{') {
+      storage.push('}');
+    } else if (s[i] !== storage.pop()) {
+      return false;
+    }
+  }
+  if (storage.length < 1) { return true; }
+  // if the string only has opening braces, then it isn't valid so return false
+  return false;
+}
+
+// console.log(validParentheses('')); // true
+// console.log(validParentheses('()')); // true
+// console.log(validParentheses('()[]{}')); // true
+// console.log(validParentheses('(]')); // false
+// console.log(validParentheses('([)]')); // false
+// console.log(validParentheses('{[]}')); // true
