c590a63482c428ff10a877836665a8936caaed1c
Brian Kim <briaansjkim@gmail.com>
2020-03-30 13:00:01 -0400

    implemented twoSum

--- leetcode/TwoSum.js
+++ leetcode/TwoSum.js
@@ -2,6 +2,17 @@
 // You may assume that each input would have EXACTLY one solution, and you may not use the same element twice
 
 // Example 1:
-// nums = [2, 7, 11, 15], target = 9
+// i: nums = [2, 7, 11, 15], target = 9
+// o: [0, 1] because nums[0] + nums[1] = target
+
+function twoSum(nums, target) {
+  for (var i = 0; i < nums.length; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
 
 
