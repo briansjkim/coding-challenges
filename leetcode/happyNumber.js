83cd0df0e5ecba38edb3ac505889698e3ae6d13d
Brian Kim <briaansjkim@gmail.com>
2020-04-06 12:55:10 -0400

    added example

--- leetcode/happyNumber.js
+++ leetcode/happyNumber.js
@@ -4,3 +4,12 @@
 // Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay),
 // or it loops endlessly in a cycle which does not include 1.
 // Those numbers for which this process ends in 1 are happy numbers
+
+// Example:
+// Input: 19
+// Output: true
+// Explanation:
+// 1^2 + 9^2 = 82
+// 8^2 + 2^2 = 69
+// 6^2 + 9^2 = 100
+// 1^2 + 0^2 + 0^2 = 1
