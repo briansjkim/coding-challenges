efcd2e20a579adbe4d49ae33eb17184cd9359e3e
Brian Kim <briaansjkim@gmail.com>
2020-04-07 13:02:02 -0400

    practice w/ maxSubArray

--- leetcode/maxSubArray.js
+++ leetcode/maxSubArray.js
@@ -1,6 +1,6 @@
 // given an integer array nums
 // find the contiguous subarray (containing at least one number) which has the largest sum
-  // and return its sum
+// and return its sum
 
 
 // i: array of numbers
@@ -10,34 +10,25 @@
 
 
 // approach: (finding the max as we iterate w/o any variables) -- better for space complexity
-  // create a function that finds the contiguous subarray which has the largest sum and return its sum
-  // iterate through the input array (start at 1 because starting at 0 will make every number NaN)
-    // assign the current number as the max between the current number and the current number plus the number before it
-  // return the max number within the array
+// create a function that finds the contiguous subarray which has the largest sum and return its sum
+// iterate through the input array (start at 1 because starting at 0 will make every number NaN)
+// assign the current number as the max between the current number and the current number plus the number before it
+// return the max number within the array
 
-// declarative solution
-var maxSubArray = (nums) => {
+
+var maxSubArray = function (nums) {
   if (nums.length === 0) {
     return 0;
   }
-  for (var i = 1; i < nums.length; i++) {
-    nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
-  }
-  return Math.max(...nums);
-}
 
-// imperative solution
-var maxSubArray = (nums) => {
-  if (nums.length === 0) {
-    return 0;
-  }
-  var maxSum = Number.NEGATIVE_INFINITY;
   var tempSum = 0;
+  var maxSum = Number.NEGATIVE_INFINITY;
 
   for (var i = 0; i < nums.length; i++) {
-    tempSum = Math.max(nums[i], nums[i] + tempSum);
-    maxSum = Math.max(maxSum, tempSum);
+    tempSum = Math.max(nums[i], tempSum + nums[i]);
+    maxSum = Math.max(tempSum, maxSum);
   }
+
   return maxSum;
 }
 
