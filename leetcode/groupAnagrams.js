1fa107c6445a600635b9dd977c691686466476ca
Brian Kim <briaansjkim@gmail.com>
2020-04-07 15:40:02 -0400

    added ioce

--- leetcode/groupAnagrams.js
+++ leetcode/groupAnagrams.js
@@ -3,3 +3,8 @@
 // Example:
 // Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
 // Output: [ ['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat'] ]
+
+// i: array
+// o: array of arrays
+// c:
+// e: all inputs will be lowercase. order of output doesn't matter
