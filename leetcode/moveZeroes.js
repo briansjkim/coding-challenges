67ed40ec1bb1eecd01f254a8e97c17a9499b9155
Brian Kim <briaansjkim@gmail.com>
2020-04-07 13:07:26 -0400

    added example

--- leetcode/moveZeroes.js
+++ leetcode/moveZeroes.js
@@ -1,1 +1,8 @@
 // Given an array nums, write a funciton to move all 0's to the end of it while maintaining the relative order of the non-zero elements
+
+// Example
+// I: [0, 1, 0, 3, 12]
+// O: [1, 3, 12, 0, 0]
+// C:
+// E: Must do this in-place without making a copy of the array
+      // Minimize the total number of operations
