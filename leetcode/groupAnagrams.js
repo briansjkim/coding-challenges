5cda473830a7ae2e7e5710398c757390839c2669
Brian Kim <briaansjkim@gmail.com>
2020-04-07 15:39:32 -0400

    added example

--- leetcode/groupAnagrams.js
+++ leetcode/groupAnagrams.js
@@ -1,1 +1,5 @@
 // Given an array of strings, group anagrams together
+
+// Example:
+// Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
+// Output: [ ['ate', 'eat', 'tea'], ['nat', 'tan'], ['bat'] ]
