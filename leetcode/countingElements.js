cac1ddd5506f4ecd345919bd2b6d21ddcf0f5f5e
Brian Kim <briaansjkim@gmail.com>
2020-04-08 18:24:59 -0400

    added example

--- leetcode/countingElements.js
+++ leetcode/countingElements.js
@@ -1,2 +1,10 @@
 // given an integer array arr, count element x such that x + 1 is also in arr
 // if there are duplicates in arr, count them separately
+
+// Example 1:
+// I: arr = [1, 2, 3]
+// O: 2
+// Explanation: 1 and 2 are counted because 2 and 3 are in arr
+// x = 1; 1 + 1 = 2 which is in arr
+// x = 2; 2 + 1 = 3 which is in arr
+// x = 3; 3 + 1 = 4. not in arr
