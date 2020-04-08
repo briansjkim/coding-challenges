d4c0f4958419a5fee780b3b216f99742bba3caac
Brian Kim <briaansjkim@gmail.com>
2020-04-08 18:27:05 -0400

    added 2nd example

--- leetcode/countingElements.js
+++ leetcode/countingElements.js
@@ -8,3 +8,8 @@
 // x = 1; 1 + 1 = 2 which is in arr
 // x = 2; 2 + 1 = 3 which is in arr
 // x = 3; 3 + 1 = 4. not in arr
+
+// Example 2:
+// I: arr = [1, 1, 3, 3, 5, 5, 7, 7]
+// O: 0
+// Explanation: No numbers are counted because there's no 2, 4, 6, 8 in arr
