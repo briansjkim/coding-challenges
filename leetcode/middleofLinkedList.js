0fad6b1b85c33f81f771c75810db6e0dd94dc2f8
Brian Kim <briaansjkim@gmail.com>
2020-04-13 12:50:49 -0400

    created test list

--- leetcode/middleofLinkedList.js
+++ leetcode/middleofLinkedList.js
@@ -23,3 +23,11 @@ function ListNode(val) {
 var middleNode = function (head) {
 
 }
+
+var list = new ListNode(1);
+list.next = new ListNode(2);
+list.next.next = new ListNode(3);
+list.next.next.next = new ListNode(4);
+list.next.next.next.next = new ListNode(5);
+
+// console.log(list);
