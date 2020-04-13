c2229b23e813c1627a23ff1791970ee71375ce6d
Brian Kim <briaansjkim@gmail.com>
2020-04-13 15:39:08 -0400

    added example

--- leetcode/backspaceStringCompare.js
+++ leetcode/backspaceStringCompare.js
@@ -1,2 +1,7 @@
-// Given two strings S and T, return if they are equal when both are typed into empty text edits.
+// Given two strings S and T, return if they are equal when both are typed into empty text editors.
 // # means a backspace character
+
+// Example 1:
+// I: S = "ab#c", T = "ad#c"
+// O: true
+// Explanation: Both S and T become "ac"
