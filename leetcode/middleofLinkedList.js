caebf972dcdb0a9627001b6785cfc87f13324666
Brian Kim <briaansjkim@gmail.com>
2020-04-13 12:46:25 -0400

    added example

--- leetcode/middleofLinkedList.js
+++ leetcode/middleofLinkedList.js
@@ -1,2 +1,6 @@
 // Given a non-empty, singly linked list with head node 'head', return a middle node of linked list
 // If there are two middle nodes, return the second middle node
+
+// Example 1:
+// I: [1, 2, 3, 4, 5]
+// O: Node 3 from this list (Serialization: [3, 4, 5])
