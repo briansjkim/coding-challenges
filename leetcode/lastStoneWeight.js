be4a76fb0def88cb275ed8e9a168c2dbf24dbf08
Brian Kim <briaansjkim@gmail.com>
2020-04-20 12:30:47 -0400

    added ioce

--- leetcode/lastStoneWeight.js
+++ leetcode/lastStoneWeight.js
@@ -15,3 +15,12 @@
 // We combine 2 and 4 to get 2 so the array converts to [2, 1, 1, 1]
 // We combine 2 and 1 to get 1 so the array converts to [1, 1, 1]
 // We combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone
+
+// Note:
+// 1 <= stones.length <= 30
+// 1 <= stones[i] <= 1000
+
+// I: array of integers
+// O: integer
+// C:
+// E:
