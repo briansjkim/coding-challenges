f9176708b7f2ab9d781ee82d4d27ae2082e96919
Brian Kim <briaansjkim@gmail.com>
2020-05-19 14:48:15 -0400

    implemented sortedArrayToBST

--- leetcode/sortedArrayToBST.js
+++ leetcode/sortedArrayToBST.js
@@ -12,7 +12,17 @@ function TreeNode(val) {
 }
 
 var sortedArrayToBST = function (nums) {
+  if (nums.length === 0) { return null; }
 
+  // since the array is already sorted, we can start with the middle number as the root
+  let mid = Math.floor(nums.length / 2);
+  let root = new TreeNode(nums[mid]);
+
+  // set the left side of the root to be the left half of the array
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  // set the right side of the root to be the right half of the array
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+  return root;
 }
 
 var nums = [-10, -3, 0, 5, 9];
