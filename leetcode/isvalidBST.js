8c44ab42ef6c44d34c485e91e4af29671d4cf480
Brian Kim <briaansjkim@gmail.com>
2020-05-19 11:53:11 -0400

    init problem

---
+++ leetcode/isvalidBST.js
@@ -0,0 +1,8 @@
+/**
+ * Given a binary tree, determine if it is a valid binary search tree
+ *
+ * Assume a BST is defined as follow:
+ *  The left subtree of a node contains only nodes with keys less than the node's key
+ *  The right subtree of a node contains only nodes with keys greater than the node's key
+ *  Both the left and right subtree must also be binary search tree
+ */
