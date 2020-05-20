a4d3cb66c120670f7264b3f170e94d4c6588b2a5
Brian Kim <briaansjkim@gmail.com>
2020-05-20 13:12:20 -0400

    added examples

--- leetcode/containsDuplicates.js
+++ leetcode/containsDuplicates.js
@@ -2,4 +2,16 @@
  * Given an array of itengers, find if the array contains any duplicates
  *
  * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct
+ *
+ * Ex 1:
+ * i: [1, 2, 3, 1]
+ * o: true
+ *
+ * Ex 2:
+ * i: [1, 2, 3, 4]
+ * o: false
+ *
+ * Ex 3:
+ * i: [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+ * o: true
  */
