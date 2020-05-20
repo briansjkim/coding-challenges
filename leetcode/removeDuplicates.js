7b5b927e0209c42247c589d3c3ae65330d6d046f
Brian Kim <briaansjkim@gmail.com>
2020-05-20 12:50:09 -0400

    practice

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -29,7 +29,21 @@ var removeDuplicates = function (nums) {
   return Object.keys(uniques).length;
 }
 
+var removeDuplicates = function (nums) {
+  let i = 0;
+  let j = 1;
 
+  while (i < nums.length && j < nums.length) {
+    if (nums[i] === nums[j]) {
+      nums.splice(j, 1);
+    } else {
+      i++;
+      j++;
+    }
+  }
+
+  return nums.length;
+}
 
 
 console.log(removeDuplicates([1, 1, 2]));
