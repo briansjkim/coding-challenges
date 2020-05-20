e546023c115ba1d1c6458c0c4ca402a144f776db
Brian Kim <briaansjkim@gmail.com>
2020-05-20 17:11:22 -0400

    practice

--- leetcode/plusOne.js
+++ leetcode/plusOne.js
@@ -1,19 +1,21 @@
 var plusOne = (digits) => {
-  // iterate from the end bc the last element is the only element that we need to (POSSIBLY) change
-  for (var i = digits.length - 1; i >= 0; i--) {
-    // pre-increment the digit first
-      // we can increment the digit to the desired result AND check if it's greater than 9
-    if (++digits[i] > 9) {
-      // if the incremented digit is greater than 9,
-      // turn it into a zero
+  // start at the end because the last element COULD be the only digit that we change
+  for (let i = digits.length - 1; i >= 0; i--) {
+    // we pre-increment the digit because regardless of whether it is greater than or equal to 10, we have to increment the last digit
+    // Ex: [1, 2, 3] -> 3 is already incremented BEFORE the condition is checked
+
+    // if the last digit was 9, then it becomes 10 and i moves left to the next element
+    // THEN it pre-increments and then checks if it's greater than or equal to 10
+
+    digits[i] += 1;
+    if (digits[i] >= 10) {
       digits[i] = 0;
     } else {
-      // return if the digit isn't greater than 9
       return digits;
     }
   }
-  // if all of the digits were greater than 9
-  // add a 1 to the beginning to match the desired result
+
+  // if all of the digits were 9 or 0, then we want to add 1 to the beginning
   digits.unshift(1);
   return digits;
 }
