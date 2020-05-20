36f3d54856d91d5284127c18d6b6847fb36a1e04
Brian Kim <briaansjkim@gmail.com>
2020-05-20 12:35:49 -0400

    added examples

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -1,7 +1,15 @@
 // given a sorted array, remove the duplicates IN-PLACE such that each element appear only ONCE and return the new length
-// do not allocate extra space for a nother array, you must do this by MODIFYING THE INPUT ARRAY in-place with O(1) extra memory
+// do not allocate extra space for another array, you must do this by MODIFYING THE INPUT ARRAY in-place with O(1) extra memory
 
-
+/**
+ * Ex 1:
+ * I: nums = [1, 1, 2]
+ * o: 2 because it should be the length of the array after ONLY considering unique values
+ *
+ * Ex 2:
+ * i: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+ * o: 5 because nums should only contain 0, 1, 2, 3, 4
+ */
 
 
 
