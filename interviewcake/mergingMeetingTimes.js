110e00b5debda0a7484947eafefd06a7ce3ab4e8
Brian Kim <briaansjkim@gmail.com>
2021-01-13 14:17:06 -0500

    added init mergingMeetingTimes

---
+++ interviewcake/mergingMeetingTimes.js
@@ -0,0 +1,24 @@
+// Your company built an in-house calendar tool called HiCal. You want to add a feature to see the times in a day when everyone is available
+// In HiCal, a meeting is stored as objects with integer properties startTime and endTime. These integers represent the number of 30-minute blocks past 9:00am.
+
+// Ex: 
+// { startTime: 2, endTime: 3 }  // meeting from 10:00 – 10:30 am
+// { startTime: 6, endTime: 9 }  // meeting from 12:00 – 1:30 pm
+
+// Given 
+// [
+//     { startTime: 0,  endTime: 1 },
+//     { startTime: 3,  endTime: 5 },
+//     { startTime: 4,  endTime: 8 },
+//     { startTime: 10, endTime: 12 },
+//     { startTime: 9,  endTime: 10 },
+// ]
+
+// your function would return 
+// [
+//     { startTime: 0, endTime: 1 },
+//     { startTime: 3, endTime: 8 },
+//     { startTime: 9, endTime: 12 },
+// ]
+
+// Don't assume the meetings are in order. The meeting times are coming from multiple teams
