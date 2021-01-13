fe16d9efc9e1ec32d8a283fa3f31a4ef4a8053d0
Brian Kim <briaansjkim@gmail.com>
2021-01-13 14:20:20 -0500

    added examples

--- interviewcake/mergingMeetingTimes.js
+++ interviewcake/mergingMeetingTimes.js
@@ -26,3 +26,33 @@
 function mergeRanges() {
 
 };
+
+// meetings overlap
+let actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }]);
+// result: [{ startTime: 1, endTime: 4 }];
+
+// meetings touch 
+actual = mergeRanges([{ startTime: 5, endTime: 6 }, { startTime: 6, endTime: 8 }]);
+// result = [{ startTime: 5, endTime: 8 }];
+
+// multiple merged meetings
+actual = mergeRanges([
+  { startTime: 1, endTime: 4 },
+  { startTime: 2, endTime: 5 },
+  { startTime: 5, endTime: 8 },
+]);
+// result = [{ startTime: 1, endTime: 8 }];
+
+actual = mergeRanges([
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 },
+]);
+
+// result = [
+//     { startTime: 0, endTime: 1 },
+//     { startTime: 3, endTime: 8 },
+//     { startTime: 9, endTime: 12 },
+//   ];
