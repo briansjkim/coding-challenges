c41aa0716ea6a235e4eef3a6c21db8b90bfeaddb
Brian Kim <briaansjkim@gmail.com>
2021-03-30 12:58:53 -0400

    added examples

--- leetcode/balancedStringSplit.js
+++ leetcode/balancedStringSplit.js
@@ -4,4 +4,24 @@
     Given a balanced string s, split it in the maximum amount of balanced strings.
 
     Return the maximum amount of split balanced strings.
+
+    Ex 1:
+    i: s = 'RLRRLLRLRL'
+    o: 4
+    'RL', 'RRLL', 'RL', 'RL'
+
+    Ex 2:
+    i: s = 'RLLLLRRRLR'
+    o: 3
+    'RL', 'LLLRRR', 'LR'
+
+    Ex 3:
+    i: s = 'LLLLRRRR';
+    o: 1
+    'LLLLRRRR';
+
+    Ex 4:
+    i: s = 'RLRRRLLRLL'
+    o: 2
+    'RL', 'RRRLLRLL'
  */
