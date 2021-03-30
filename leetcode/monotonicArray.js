586c7bc07d3ae47ead85e88e9f62922fb6c427fb
Brian Kim <briaansjkim@gmail.com>
2021-03-30 12:50:55 -0400

    added examples Please enter the commit message for your changes. Lines starting

--- leetcode/monotonicArray.js
+++ leetcode/monotonicArray.js
@@ -4,4 +4,24 @@
  * An array A is monotone increasing if for all i <= j; A[i] <= A[j]. An array A is monotone decreasing if for all i <= j, A[i] >= A[j]
  * 
  * Return true if and only if the given array A is monotonic
+ * 
+ * Ex 1) 
+ * I: [1, 2, 2, 3]
+ * O: true
+ * 
+ * Ex 2:
+ * I: [6, 5, 4, 4]
+ * O: true
+ * 
+ * Ex 3:
+ * I: [1, 3, 2]
+ * O; [false]
+ * 
+ * Ex 4:
+ * i: [1, 2, 4, 5]
+ * o: true
+ * 
+ * Ex 5:
+ * i: [1, 1, 1]
+ * o: true
  */
