44d8434d62983b3b01067c2d9a9f09d912c3749d
Brian Kim <briaansjkim@gmail.com>
2021-03-31 14:12:52 -0400

    completed problem

--- leetcode/areAlmostEqual.js
+++ leetcode/areAlmostEqual.js
@@ -23,3 +23,46 @@
     Input: s1 = "abcd", s2 = "dcba"
     Output: false
  */
+
+var areAlmostEqual = function(s1, s2) {
+    if (s1.length !== s2.length) {
+        return false;
+    };
+
+    let i = 0;
+    let j = 0;
+    let s1Obj = {};
+    let s2Obj = {};
+    let count = 0;
+
+    for (i; i < s1.length; i++) {
+        s1Obj[s1[i]] = (s1Obj[s1[i]] || 0 ) + 1;
+    };
+
+    for (j; j < s2.length; j++) {
+        s2Obj[s2[j]] = (s2Obj[s2[j]] || 0 ) + 1;
+    };
+
+    for (let key in s1Obj) {
+        if (!s2Obj.hasOwnProperty(key)) {
+            return false;
+        };
+
+        if (s1Obj[key] !== s2Obj[key]) {
+            return false;
+        };
+    };
+
+    i = 0;
+    j = 0;
+
+    while (i < s1.length && j < s2.length) {
+        if (s1[i] !== s2[j]) {
+            count++;
+        };
+        i++;
+        j++;
+    };
+
+    return count === 2 || count === 0;
+};
