da68ee1d1297ad3eafdb395e210df43500272771
Brian Kim <briaansjkim@gmail.com>
2021-04-01 11:40:27 -0400

    init problem with examples

---
+++ leetcode/palindromeLinkedList.js
@@ -0,0 +1,15 @@
+/**
+ * Given head of a singly linked list, return true if it is a palindrome
+ * 
+ * Ex 1:
+ * I: head = [1, 2, 2, 1]
+ * o: true
+ * 
+ * Ex 2:
+ * i: head = [1, 2]
+ * o: false
+ * 
+ * Constraints:
+ * The number of nodes in the list is in the range [1, 10^5]
+ * 0 <= Node.val <= 9
+ */
