d464a98a96d0c80f6f19dee5a99ecfdff6ddf35b
Brian Kim <briaansjkim@gmail.com>
2021-04-01 11:43:10 -0400

    added initial solution

--- leetcode/palindromeLinkedList.js
+++ leetcode/palindromeLinkedList.js
@@ -13,3 +13,39 @@
  * The number of nodes in the list is in the range [1, 10^5]
  * 0 <= Node.val <= 9
  */
+
+// not optimal for time
+var reverse = function(node) {
+    let currentNode = node;
+    let prevNode = null;
+    let nextNode = null;
+
+    while (currentNode) {
+        nextNode = currentNode.next;
+        prevNode = currentNode;
+
+        currentNode.next = prevNode;
+        currentNode = nextNode;
+    };
+
+    // return the prevNode because currentNode will fall out of the LL while the prevNode will be at the last node
+    return prevNode;
+};
+
+var isPalindrome = function(head) {
+    if (head === null || head.next === null) {
+        return true;
+    };
+
+    let reversedList = reverse(head);
+
+    while (head && reversedList) {
+        if (head.val !== reversedList.val) {
+            return false;
+        };
+    };
+
+    return true;
+};
+
+
