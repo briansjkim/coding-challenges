a94c5a5c919b28c0b8653a7dd2b34bf05e682a3f
Brian Kim <briaansjkim@gmail.com>
2021-04-01 13:34:35 -0400

    added previous solution back

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -53,3 +53,38 @@ var addTwoNumbers = function(l1, l2) {
   return newList.next;
 };
 // runtime: 136ms, memory usage: 44.2MB
+
+// previous solution
+var addTwoNumbers = (l1, l2) => {
+  // create a new LL
+  let dummyHead = new ListNode(-1);
+  // set a pointer at the new LL's head
+  // this is what we'll be using to return at the end while the LL continues to grow
+  let newHead = dummyHead;
+  let sum = 0;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || sum > 0) {
+    if (l1 !== null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2 !== null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    if (sum >= 10) {
+      sum -= 10;
+      carry = 1;
+    }
+
+    dummyHead.next = new ListNode(sum);
+    dummyHead = dummyHead.next;
+    sum = carry;
+    carry = 0;
+  }
+
+  return newHead.next;
+}
