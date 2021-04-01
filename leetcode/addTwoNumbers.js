685624d27c94d0379154a2370b72e09984b26451
Brian Kim <briaansjkim@gmail.com>
2021-04-01 13:33:45 -0400

    completed problem

--- leetcode/addTwoNumbers.js
+++ leetcode/addTwoNumbers.js
@@ -16,4 +16,39 @@ function ListNode(val) {
   this.next = null;
 }
 
+var addTwoNumbers = function(l1, l2) {
+  let newList = new ListNode(-1);
+  let newHead = newList;
+  let sum = 0;
+  let carry = 0;
 
+  // we check if the current node for each LL is valid because the two LLs could be unequal in length
+  // we also check if the sum is greater than 0 because we have to add it to the end of the list
+  while (l1 || l2 || sum > 0) {
+    // in the case that the two LLs aren't equal, we again check if the current node for each LL is valid
+    // we do this again because the while loop will run if any of the three conditions are true, so we might have reached the end of l1 and l2 but sum > 0, so we don't
+    // want to add the nonvalid values from l1 and l2 which will be undefined/null
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    };
+
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.val;
+    };
+
+    if (sum >= 10) {
+      sum -= 10;
+      carry = 1;
+    };
+
+    newHead.next = new ListNode(sum);
+    newHead = newHead.next;
+    // assign carry to sum so that we can add it with the next sum
+    sum = carry;
+    carry = 0;
+  };
+
+  return newList.next;
+};
