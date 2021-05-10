ae543902adab13e3aa5c13c426a6c57252bba621
Brian Kim <briaansjkim@gmail.com>
2021-05-10 15:15:38 -0400

    solved reverse

--- interviewcake/reverseString.js
+++ interviewcake/reverseString.js
@@ -1,7 +1,23 @@
 // Write a function that takes an array of characters and reverses the letters in place
 
 function reverse(arrayOfChars) {
+    // nothing to reverse if the length is equal to or less than 1
+    if (arrayOfChars.length <= 1) {
+        return arrayOfChars;
+    };
 
+    let startIdx = 0;
+    let endIdx = arrayOfChars.length - 1;
+
+    while (startIdx < endIdx) {
+        let temp = arrayOfChars[startIdx];
+        arrayOfChars[startIdx] = arrayOfChars[endIdx];
+        arrayOfChars[endIdx] = temp;
+        startIdx++;
+        endIdx--;
+    };
+
+    return arrayOfChars;
 };
 
 let input = 'ABCDE'.split('');
