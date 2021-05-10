97124f4b3b2c5ddc23e7190b50e8be570e73fa95
Brian Kim <briaansjkim@gmail.com>
2021-05-10 14:48:37 -0400

    init reverseString

---
+++ interviewcake/reverseString.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array of characters and reverses the letters in place
+
+function reverse(arrayOfChars) {
+
+};
+
+let input = 'ABCDE'.split('');
+console.log(reverse(input)); // ['E', 'D', 'C', 'B', 'A'] => 'EDCBA'
