8d28cf08b7006f806d4ab44914a43020313c9ac4
Brian Kim <briaansjkim@gmail.com>
2021-05-11 15:47:16 -0400

    init problem

---
+++ interviewcake/palindromeCheck.js
@@ -0,0 +1,5 @@
+/**
+ * A string is a palindrome if it is read the same from forward or backward.
+ * 
+ * For example, dad reads the same either from forward or backward. So the word dad is a palindrome. Similarly, madam is also a palindrome.
+ */
