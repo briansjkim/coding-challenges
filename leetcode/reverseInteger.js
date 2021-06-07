f7766409cc9856cef30027f448dabff1941a1cb1
Brian Kim <briaansjkim@gmail.com>
2021-06-07 13:22:49 -0400

    solved problem in a different way

--- leetcode/reverseInteger.js
+++ leetcode/reverseInteger.js
@@ -43,3 +43,13 @@ var reverse = function(x) {
     return result * Math.sign(x);
   };
 };
+
+var reverse = function(x) {
+  let reversedNum = Math.abs(x).toString().split('').reverse().join('');
+
+  if (reversedNum > 2**31) {
+    return 0;
+  } else {
+    return reversedNum * Math.sign(x);
+  };
+};
